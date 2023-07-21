f25e95faf2ccedad65d8ae28c00b148471cda911
RPy-Mon <131710822+RPy-Mon@users.noreply.github.com>
2023-07-21 02:11:04 -0700

    Update fs.js

--- tools/fs.js
+++ tools/fs.js
@@ -1,13 +1,16 @@
-const rp_fs = {
-    read: (path) => {
+rp_fs = {
+    read: (path, callback) => {
         renpy_exec(`with open('` + path + `', 'r') as f:
-    print(f.read())`);
+    globals()["file_output"] = f.read()`);
+        renpy_get("file_output").then(callback)
     },
     write: (path, data) => {
         renpy_exec(`with open('` + path + `', 'w') as f:
     f.write('` + data + `')`);
     },
-    look: (path) => {
-        renpy_exec('print(os.listdir(\'' + path + '\'))');
+    look: (path, callback) => {
+        renpy_set("files", [])
+        renpy_exec('globals()["files"] = os.listdir(\'' + path + '\')');
+        renpy_get("files").then(callback)
     }
 }
